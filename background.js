6705a481f82d765a6999348328d61859fb6bc98e
Shinyi Ouyang <shiouya@nus11150-10-shiouya.hsd1.ca.comcast.net>
2018-11-09 23:56:53 -0800

    add buildings

--- background.js
+++ background.js
@@ -1,95 +1,44 @@
-function getHostname(url) {
-    //getHostname("https://en.wikipedia.org/wiki/Holy_Roman_Empire") => 
-    //"en.wikipedia.org"
-    const hostname = url.match(/:\/\/(?:www[1-9]?\.)?(.[^\/:]+)/)
-    if (Array.isArray(hostname) === true && hostname.length === 2) {
-        return hostname[1];
-    } else {
-        return null;
-    }
-}
-function setTerritories(amount, callback = () => { }) {
-    if (!isNaN(amount)) {
-        chrome.storage.local.set({ territories: amount })
-        callback();
-    }
-    else {
-        //placeholder
-    }
-}
 function addTerritories(amount, callback = () => { }) {
     if (!isNaN(amount)) {
         chrome.storage.local.get('territories', function (result) {
-            new_amount = result.territories + amount
-            chrome.storage.local.set({ territories: new_amount })
-            callback(new_amount);
+            let newAmount = result.territories + amount
+            console.log(newAmount)
+            chrome.storage.local.set({ territories: newAmount })
+            callback(newAmount);
         });
     }
     else {
         //placeholder
     }
 }
-function subtractTerritories(amount, callback1 = () => { }, callback2 = () => { }) {
-    if (!isNaN(amount)) {
-        chrome.storage.local.get('territories', function (result) {
-            if (amount > result.territories){
-                callback2(amount);
+function checkStorage(items){
+    //makes sure keys exist in chrome storage and creates them if they do not exist
+    //takes an obj with the keys to be checked, 
+    //and the value to set if the keys do not exist
+    chrome.storage.local.get(Object.keys(items), function (result){
+        let setObj = {}
+        for (key of Object.keys(items)){
+            if (result.hasOwnProperty(key) === false) {
+                setObj[key] = items[key]
             }
-            else{
-                new_amount = result.territories + amount
-                chrome.storage.local.set({ territories: new_amount })
-                callback1(new_amount);
-            }
-        });
-    }
-    else {
-        //placeholder
-    }
-}
-function getTerritories(callback = () => { }) {
-    chrome.storage.local.get('territories', function (result) {
-        console.log(result.territories);
-        callback(result.territories)
-        //return result.tabcoins;
+        }
+        chrome.storage.local.set(setObj)
     });
-}
-function checkStorage(items, new_value = 0) {
-    //checkStorage can accept strings and array of strings
-    //makes sure values exist in chrome storage and creates them if they do not exist
-    //eventually will be changed to object instead of array
-    if (Object.prototype.toString.call(items) === "[object String]") {
-        chrome.storage.local.get(items, function (result) {
-            if (Object.keys(result).length === 0 && result.constructor === Object) {
-                chrome.storage.local.set({ [items]: new_value });
-            }
-        });
-    }
-    else {
-        chrome.storage.local.get(items, function (result) {
-            var create_obj = {}
-            for (item of items) {
-                if (result.hasOwnProperty(item) === false) {
-                    create_obj[item] = new_value;
-                }
-            }
-            chrome.storage.local.set(create_obj);
-        });
-    }
 }
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     //TBD later
     if (tab.status === "complete") {
         addTerritories(1);
-        // chrome.storage.local.get('visited_hostnames', function (result) {
+        // chrome.storage.local.get('visitedHostnames', function (result) {
         //     //console.log(result)
-        //     let visited_hostnames = result.visited_hostnames
-        //     var current_hostname = getHostname(tab.url)
-        //     //console.log(visited_hostnames)
-        //     //console.log(current_hostname)
-        //     if (!visited_hostnames.includes(current_hostname)) {
+        //     let visitedHostnames = result.visitedHostnames
+        //     var currentHostname = getHostname(tab.url)
+        //     //console.log(visitedHostnames)
+        //     //console.log(currentHostname)
+        //     if (!visitedHostnames.includes(currentHostname)) {
         //         //console.log("url match not found");
-        //         visited_hostnames.push(current_hostname);
-        //         chrome.storage.local.set({ visited_hostnames: visited_hostnames })
+        //         visitedHostnames.push(currentHostname);
+        //         chrome.storage.local.set({ visitedHostnames: visitedHostnames })
         //     } else {
         //         //console.log("url match found")
         //     }
@@ -97,27 +46,38 @@ chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     }
 });
 chrome.runtime.onInstalled.addListener(function () {
-    checkStorage("territories", 0);
-    checkStorage("last_update", Date.now());
-    checkStorage("buildings", {
-        "scout": 0, "settler": 0, "man-at-arms": 0, "soldier": 0, "overlord": 0,
-        "merchant": 0, "galleon": 0, "flag-maker": 0, "colonial_offices": 0, "plane": 0, "diplomat": 0,
-        "administrative_offices": 0, "spaceship": 0, "warp_field": 0, "portal": 0, "the_machine": 0
-    })
+    checkStorage({"territories": 50});
+    checkStorage({"lastUpdate": Date.now()});
+    checkStorage({"buildings": {
+        "scout": 0, "settler": 0, "manAtArms": 0, "soldier": 0, "overlord": 0,
+        "merchant": 0, "galleon": 0, "flagMaker": 0, 
+        // "colonialOffice": 0, "diplomat": 0, "plane": 0, "administrativeOffice": 0 
+        //"spaceship": 0, "warpField": 0, "portal": 0, "theMachine": 0
+    }})
 });
 // function getTabUrls() {
 //     chrome.tabs.query({}, function (tabs) {
 //         const taburls = tabs.map(tab => tab.url);
 //         chrome.storage.local.set({ urls: taburls });
-//         chrome.storage.local.set({ visited_hostnames: taburls.map(url => getHostname(url)) });
+//         chrome.storage.local.set({ visitedHostnames: taburls.map(url => getHostname(url)) });
 //     });
 // }
 // function getWindows() {
-//     chrome.windows.getAll({ populate: true }, function (all_windows) {
+//     chrome.windows.getAll({ populate: true }, function (allWindows) {
 //         const windowids = {}
-//         for (windows of all_windows) {
+//         for (windows of allWindows) {
 //             windowids[windows.id] = windows;
 //         }
 //         chrome.storage.local.set({ windowids: windowids })
 //     })
+// }
+// function getHostname(url) {
+//     //getHostname("https://en.wikipedia.org/wiki/Holy_Roman_Empire") => 
+//     //"en.wikipedia.org"
+//     const hostname = url.match(/:\/\/(?:www[1-9]?\.)?(.[^\/:]+)/)
+//     if (Array.isArray(hostname) === true && hostname.length === 2) {
+//         return hostname[1];
+//     } else {
+//         return null;
+//     }
 // }
--- territorystats.js
+++ territorystats.js
@@ -1,43 +1,168 @@
-function getHostname(url) {
-    //getHostname("https://en.wikipedia.org/wiki/Holy_Roman_Empire") => 
-    //"en.wikipedia.org"
-    const hostname = url.match(/:\/\/(?:www[1-9]?\.)?(.[^\/:]+)/);
-    if (Array.isArray(hostname) === true && hostname.length === 2) {
-        return hostname[1];
+class Building {
+    constructor(name, basecost, tps, id = false) {
+        //move basecost and amount out of Building 
+        this.quantity;
+        this.cost;
+        this.basecost = basecost;
+        this.tps = tps;
+        this.name = name;
+        var child = document.createElement("button");
+        if (id === false) {
+            this.id = name;
+            child.id = name;
+        }
+        else {
+            this.id = id;
+            child.id = id
+        }
+        child.textContent = name;
+        child.className = "buyButton";
+        this.child = child;
+        document.body.appendChild(child);
+    }
+    getCost(callback = () => { }) {
+        chrome.storage.local.get('buildings', function (result) {
+            const cost = this.basecost * (result.buildings[this.id] + 1);
+            callback(cost);
+        })
+    }
+}
+function setTerritories(amount, callback = () => { }) {
+    if (!isNaN(amount)) {
+        chrome.storage.local.set({ territories: amount })
+        callback();
     }
     else {
-        return null;
+        //placeholder
     }
 }
-function recalculateTPS() {
-    //will recalculate the territories per second 
-    //only time when upgrades/buildings are bought
+function addTerritories(amount, callback = () => { }) {
+    if (!isNaN(amount)) {
+        chrome.storage.local.get('territories', function (result) {
+            let newAmount = Math.round(result.territories + amount);
+            chrome.storage.local.set({ territories: newAmount })
+            callback(newAmount);
+        });
+    }
+    else {
+        //placeholder
+    }
 }
-const territories_count = document.getElementById("territory_counter")
+function subtractTerritories(amount, callback1 = () => { }, callback2 = () => { }) {
+    if (!isNaN(amount)) {
+        chrome.storage.local.get('territories', function (result) {
+            if (amount > result.territories){
+                callback2(amount);
+            }
+            else{
+                let newAmount = result.territories + amount
+                chrome.storage.local.set({ territories: newAmount })
+                callback1(newAmount);
+            }
+        });
+    }
+    else {
+        //placeholder
+    }
+}
+function init() {
+    building.scout = new Building("scout", 50, 1);
+    building.settler = new Building("settler", 400, 10);
+    building.manAtArms = new Building("man at arms", 25000, 50, "manAtArms");
+    //plural of "man at arms" is "men at arms"
+    building.soldier = new Building("soldier", 10000, 240);
+    building.overlord = new Building("overlord", 30000, 1000);
+    building.merchant = new Building("merchant", 100000, 5000);
+    building.galleon = new Building("galleon", 450000, 20000);
+    building.flagMaker = new Building("flag maker", 9999999, 1234567, "flagMaker");
+    chrome.storage.local.get("buildings", function (result) {
+        tps = 0;
+        for (buildings of Object.keys(result.buildings)) {
+            let key = buildings;
+            let amount = result.buildings[key];
+            let currentBuilding = building[key]
+            tps += (amount * currentBuilding.tps);
+            currentBuilding.quantity = amount;
+            currentBuilding.cost = currentBuilding.basecost * (amount + 1);
+            document.getElementById(currentBuilding.id).addEventListener("click", function () {
+                chrome.storage.local.get("territories", function (result) {
+                    if (result.territories >= currentBuilding.cost) {
+                        chrome.storage.local.get("buildings", function (result) {
+                            let newBuildings = result.buildings;
+                            newBuildings[key] = newBuildings[key] + 1;
+                            chrome.storage.local.set({ "buildings": newBuildings })
+                            setTerritories(result.territories -= currentBuilding.cost);
+                            recalculateTPS();
+                        });
+                    }
+                });
+            });
+        }
+        let tpsCounter = document.getElementById("tpsCounter");
+        tpsCounter.textContent = "TPS: " + tps;
+        interval = setInterval(function(){
+            addTerritories(tps);
+        }, 1000);
+    });
+}
+function recalculateTPS(callback = () => { }) {
+    chrome.storage.local.get("buildings", function (result) {
+        tps = 0;
+        for (key of Object.keys(result.buildings)) {
+            tps += (result.buildings[key] * building[key].tps);
+        }
+        console.log(tps);
+        let tpsCounter = document.getElementById("tpsCounter");
+        tpsCounter.textContent = "TPS: " + tps;
+        clearInterval(interval);
+        interval = setInterval(function(){
+            addTerritories(tps);
+        }, 1000);
+        callback(tps);
+    });
+}
+
+var building = {};
+var tps;
+var interval; 
+init();
 chrome.storage.local.get("territories", function (result) {
-    //displays the amount of territoriesx
     const territories = result.territories;
-    chrome.storage.local.get("last_update", function (result) {
+    const territoriesCount = document.getElementById("territoryCounter")
+    chrome.storage.local.get("lastUpdate", function (result) {
         //updates the amount of territories based on amount of time spent offline
         //technically you don't get cookies until you open up the popup
-        if (Object.prototype.toString.call(result.last_update) === "[object Number]"){
-            const time_passed = Date.now()-result.last_update;
-            const cookies_earned = Math.round(time_passed/1000 * 0);
-            const new_cookies = cookies_earned + territories;
-            chrome.storage.local.set({last_update: Date.now(), territories: new_cookies});
-            territories_count.textContent = "Territories: " + new_cookies;
+        if (Object.prototype.toString.call(result.lastUpdate) === "[object Number]") {
+            const timePassed = Date.now() - result.lastUpdate;
+            const cookiesEarned = Math.round(timePassed / 1000 * tps);
+            const newCookies = cookiesEarned + territories;
+            chrome.storage.local.set({ lastUpdate: Date.now(), territories: newCookies });
+            territoriesCount.textContent = "Territories: " + newCookies;
         }
-        else{
-            territories_count.textContent = "Territories: " + territories;
+        else {
+            territoriesCount.textContent = "Territories: " + territories;
         }
     });
 });
 chrome.storage.onChanged.addListener(function (changed, area) {
     if (changed.hasOwnProperty("territories")) {
-        const changing_counter = document.getElementById("territory_counter");
-        changing_counter.textContent = "Territories: " + changed.territories.newValue;
+        const changingCounter = document.getElementById("territoryCounter");
+        changingCounter.textContent = "Territories: " + changed.territories.newValue;
+    }
+    if (changed.hasOwnProperty("buildings")) {
+
     }
 });
-
+// function getHostname(url) {
+//     //getHostname("https://en.wikipedia.org/wiki/Holy_Roman_Empire") => 
+//     //"en.wikipedia.org"
+//     const hostname = url.match(/:\/\/(?:www[1-9]?\.)?(.[^\/:]+)/);
+//     if (Array.isArray(hostname) === true && hostname.length === 2) {
+//         return hostname[1];
+//     }
+//     else {
+//         return null;
+//     }
+// }
 
 
