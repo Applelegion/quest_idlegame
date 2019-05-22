7f14afccc85f2033faef9c1d0e1763bfce3015b5
Shinyi Ouyang <ouyang.shinyi@gmail.com>
2019-05-21 18:10:08 -0700

    add save exporting

--- background.js
+++ background.js
@@ -14,7 +14,7 @@ function checkStorage(items) {
     //and the value to set if the keys do not exist
     chrome.storage.local.get(Object.keys(items), function (result) {
         let setObj = {}
-        for (key of Object.keys(items)) {
+        for (let key of Object.keys(items)) {
             if (result.hasOwnProperty(key) === false) {
                 setObj[key] = items[key]
             }
@@ -24,23 +24,29 @@ function checkStorage(items) {
 }
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
     //TBD later
-    if (tab.status === "complete") {
+    //OnUpdated gets changed by a multitude of factors, including google hangouts...
+    //Need to implement way to detect only the url changing or the page being refreshed
+    if (changeInfo.hasOwnProperty("url")){
         addTerritories(1);
-        // chrome.storage.local.get('visitedHostnames', function (result) {
-        //     //console.log(result)
-        //     let visitedHostnames = result.visitedHostnames
-        //     var currentHostname = getHostname(tab.url)
-        //     //console.log(visitedHostnames)
-        //     //console.log(currentHostname)
-        //     if (!visitedHostnames.includes(currentHostname)) {
-        //         //console.log("url match not found");
-        //         visitedHostnames.push(currentHostname);
-        //         chrome.storage.local.set({ visitedHostnames: visitedHostnames })
-        //     } else {
-        //         //console.log("url match found")
-        //     }
-        // });
     }
+    // if (tab.status === "complete") {
+    //     console.log(tab);
+    //     addTerritories(1);
+    //     chrome.storage.local.get('visitedHostnames', function (result) {
+    //         //console.log(result)
+    //         let visitedHostnames = result.visitedHostnames
+    //         var currentHostname = getHostname(tab.url)
+    //         //console.log(visitedHostnames)
+    //         //console.log(currentHostname)
+    //         if (!visitedHostnames.includes(currentHostname)) {
+    //             //console.log("url match not found");
+    //             visitedHostnames.push(currentHostname);
+    //             chrome.storage.local.set({ visitedHostnames: visitedHostnames })
+    //         } else {
+    //             //console.log("url match found")
+    //         }
+    //     });
+    // }
 });
 function beginApocalypse(changed, area) {
     if (changed.hasOwnProperty("buildings")) {
@@ -61,9 +67,10 @@ function beginApocalypse(changed, area) {
     }
 }
 
+4
 chrome.runtime.onInstalled.addListener(function () {
-    checkStorage({ "territories": 50 });
-    checkStorage({ "lastUpdate": Date.now() });
+    checkStorage({ territories: 50 });
+    checkStorage({ lastUpdate: Date.now() });
     checkStorage({
         "buildings": {
             "scout": 0, "settler": 0, "manAtArms": 0, "soldier": 0, "overlord": 0,
@@ -71,7 +78,9 @@ chrome.runtime.onInstalled.addListener(function () {
             // "colonialOffice": 0, "diplomat": 0, "plane": 0, "administrativeOffice": 0 
             //"spaceship": 0, "warpField": 0, "portal": 0, "theMachine": 0
         }
-    })
+    });
+    checkStorage({ upgrades: []});
+    checkStorage({ beginningOfEpoch: Date.now()});
     chrome.storage.onChanged.addListener(beginApocalypse);
 });
 // function getTabUrls() {
--- territorystats.js
+++ territorystats.js
@@ -11,7 +11,7 @@ class Item {
         }
         else {
             this.id = id;
-            this.child.id = id
+            this.child.id = id;
         }
         this.child.textContent = name;
         this.child.className = "buyButton";
@@ -21,16 +21,7 @@ class Item {
         chrome.storage.local.get('buildings', function (result) {
             const cost = this.basecost * (result.buildings[this.id] + 1);
             callback(cost);
-        })
-    }
-}
-function setTerritories(amount, callback = () => { }) {
-    if (!isNaN(amount)) {
-        chrome.storage.local.set({ territories: amount })
-        callback();
-    }
-    else {
-        //placeholder
+        });
     }
 }
 function addTerritories(amount, callback = () => { }) {
@@ -48,10 +39,10 @@ function addTerritories(amount, callback = () => { }) {
 function subtractTerritories(amount, callback1 = () => { }, callback2 = () => { }) {
     if (!isNaN(amount)) {
         chrome.storage.local.get('territories', function (result) {
-            if (amount > result.territories){
+            if (amount > result.territories) {
                 callback2(amount);
             }
-            else{
+            else {
                 let newAmount = result.territories + amount
                 chrome.storage.local.set({ territories: newAmount })
                 callback1(newAmount);
@@ -69,46 +60,64 @@ function recalculateTPS(callback = () => { }) {
             tps += (result.buildings[key] * item[key].tps);
         }
         let tpsCounter = document.getElementById("tpsCounter");
-        tpsCounter.textContent = "TPS: " + tps;
+        tpsCounter.textContent = "TPS: " + numCommas(tps);
         callback(tps);
     });
 }
-function updateButtons(){
+function updateButtons() {
     chrome.storage.local.get("buildings", function (result) {
-        for (var key of Object.keys(result.buildings)){
+        for (var key of Object.keys(result.buildings)) {
             let currentItem = item[key];
-            let cost = currentItem.basecost * (result.buildings[key] + 1);
+            let cost = numCommas(Math.round(currentItem.basecost * PRICE_GROWTH ** result.buildings[key]));
             item[key].child.textContent = currentItem.name + ":" + result.buildings[key] + ":" + currentItem.tps + ":" + cost;
         }
     })
 }
+function createUpgradeButton(id, cost) {
+    let currentButton = document.createElement("button");
+    currentButton.id = id;
+    currentButton.textContent = id + ": " + cost;
+    document.getElementById("upgrade_div").appendChild(currentButton)
+    currentButton.onclick = function () {
+        chrome.storage.local.get("territories", function (result) {
+            if (result.territories >= cost) {
+                document.getElementById("upgrade_div").removeChild(currentButton)
+                chrome.storage.local.get("upgrades", function (result) {
+                    result.upgrades.append(id);
+                    chrome.storage.local.set({ "upgrades": result.upgrades });
+                })
+            }
+        })
+    }
+}
+function numCommas(x) {
+    return x.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+}
 function init() {
-
-    const PRICE_GROWTH = 1.1;
-    item.scout = new Item("scout", 50, 1);
-    item.settler = new Item("settler", 400, 10);
-    item.manAtArms = new Item("man at arms", 2500, 50, "manAtArms");
+    item.scout = new Item("scout", 5 * 10**1, 1);
+    item.settler = new Item("settler", 4 * 10**2, 10);
+    item.manAtArms = new Item("man at arms", 2.5 * 10**3, 50, "manAtArms");
     //plural of "man at arms" is "men at arms"
-    item.soldier = new Item("soldier", 10000, 240);
-    item.overlord = new Item("overlord", 30000, 1000);
-    item.merchant = new Item("merchant", 100000, 5000);
-    item.galleon = new Item("galleon", 450000, 20000);
-    item.flagMaker = new Item("flag maker", 9999999, 1234567, "flagMaker");
-
-    document.getElementById("reset").onclick = function (){
+    item.soldier = new Item("soldier", 6.6 * 10**4, 240);
+    item.overlord = new Item("overlord", 3 * 10**5, 1000);
+    item.merchant = new Item("merchant", 3.3 * 10**6, 5000);
+    item.galleon = new Item("galleon", 7.5 * 10**7, 20000);
+    item.flagMaker = new Item("flag maker", 5 * 10**8, 1234567, "flagMaker");
+    
+    document.getElementById("reset").onclick = function () {
         //reset function
         chrome.storage.local.set({
-            territories: 25000, 
-            lastUpdate: Date.now(), 
+            territories: 0,
+            lastUpdate: Date.now(),
             "buildings": {
-                "scout": 0, "settler": 0, "manAtArms": 0, "soldier": 0, 
-                "overlord": 0,"merchant": 0, "galleon": 0, "flagMaker": 0,
+                "scout": 0, "settler": 0, "manAtArms": 0, "soldier": 0,
+                "overlord": 0, "merchant": 0, "galleon": 0, "flagMaker": 0,
             },
-        }, function (result){
+        }, function (result) {
             recalculateTPS();
             updateButtons();
         });
-    } 
+    }
     var tps;
     chrome.storage.local.get("buildings", function (result) {
         tps = 0;
@@ -117,17 +126,17 @@ function init() {
             let amount = result.buildings[key];
             let currentItem = item[key]
             tps += (amount * currentItem.tps);
-            let cost = Math.round(currentItem.basecost * (PRICE_GROWTH ** (amount)));
+            let cost = numCommas(Math.round(currentItem.basecost * PRICE_GROWTH ** amount));
             currentItem.child.textContent = currentItem.name + ":" + amount + ":" + currentItem.tps + ":" + cost;
-            function buy () {
+            function buy() {
                 // Button Listener function
                 chrome.storage.local.get(["territories", "buildings"], function (result) {
-                    let cost = currentItem.basecost * (result.buildings[key] + 1);
+                    let cost = Math.round(currentItem.basecost * (PRICE_GROWTH ** (result.buildings[key])))
                     if (result.territories >= cost) {
                         result.buildings[key] = (result.buildings[key] + 1);
                         chrome.storage.local.set({ "buildings": result.buildings });
-                        item[key].child.textContent = currentItem.name + ":" + result.buildings[key] + ":" + currentItem.tps + ":" + Math.round(currentItem.basecost * (PRICE_GROWTH ** (result.buildings[key])));
-                        setTerritories(result.territories - cost);
+                        item[key].child.textContent = currentItem.name + ":" + result.buildings[key] + ":" + currentItem.tps + ":" + numCommas(Math.round(currentItem.basecost * PRICE_GROWTH ** result.buildings[key]));
+                        chrome.storage.local.set({ territories: result.territories - cost });
                         recalculateTPS();
                         document.getElementById(currentItem.id).addEventListener("click", buy);
                     }
@@ -136,10 +145,10 @@ function init() {
             document.getElementById(currentItem.id).addEventListener("click", buy);
         }
         let tpsCounter = document.getElementById("tpsCounter");
-        tpsCounter.textContent = "TPS: " + tps;
-        let interval = setInterval(function(){
-            recalculateTPS(function(result){
-                chrome.storage.local.set({lastUpdate: Date.now()})
+        tpsCounter.textContent = "TPS: " + numCommas(tps);
+        let interval = setInterval(function () {
+            recalculateTPS(function (result) {
+                chrome.storage.local.set({ lastUpdate: Date.now() })
                 addTerritories(result);
             });
         }, 1000);
@@ -155,20 +164,62 @@ function init() {
             const cookiesEarned = Math.round(timePassed / 1000) * tps;
             const newCookies = cookiesEarned + territories;
             chrome.storage.local.set({ lastUpdate: Date.now(), territories: newCookies });
-            territoriesCount.textContent = "Territories: " + newCookies;
+            territoriesCount.textContent = "Territories: " + numCommas(newCookies);
         }
         else {
-            territoriesCount.textContent = "Territories: " + territories;
+            territoriesCount.textContent = "Territories: " + numCommas(territories);
         }
     });
-    chrome.storage.onChanged.addListener(function (changed, area) {
+    chrome.storage.onChanged.addListener(function (changed) {
         if (changed.hasOwnProperty("territories")) {
             const changingCounter = document.getElementById("territoryCounter");
-            changingCounter.textContent = "Territories: " + changed.territories.newValue;
+            changingCounter.textContent = "Territories: " + numCommas(changed.territories.newValue);
         }
     });
 }
-var item = {}; 
+function exportSave(){
+    chrome.storage.local.get(["territories", "buildings", "upgrades", "lastUpdate", "beginningOfEpoch"], function(result){
+        let base64String = window.btoa(JSON.stringify(result));
+        let temporarySaveDiv = document.createElement("a");
+        temporarySaveDiv.textContent = "download file";
+        temporarySaveDiv.setAttribute('href', 'data:text/plain;charset=utf-u,'+encodeURIComponent(base64String));
+        temporarySaveDiv.setAttribute('download', "Territory_save_file");
+        document.body.insertBefore(temporarySaveDiv, document.getElementById("exportButton").nextSibling);
+    });
+}
+function importSave(){
+    if (document.getElementById("importSelector") === null){
+        let inputElement = document.createElement("input");
+        inputElement.type = "file";
+        inputElement.id = "importSelector";
+        inputElement.accept=".txt";
+        inputElement.addEventListener("change", function() {
+            let file_to_read = document.getElementById("importSelector").files[0];
+            let fileread = new FileReader();
+            fileread.onload = function(e) {
+                importValue = JSON.parse(window.atob(e.target.result));
+            };
+            fileread.readAsText(file_to_read);
+            let submitButton = document.createElement("button");
+            submitButton.textContent = "Submit";
+            submitButton.id = "submitButton";
+            submitButton.onclick = function(){
+                chrome.storage.local.set(importValue);
+                document.body.removeChild(document.getElementById("importSelector"));
+                document.body.removeChild(document.getElementById("submitButton"));
+            };
+            document.body.appendChild(submitButton);
+        });
+        document.body.appendChild(inputElement);
+    }
+    
+}
+
+const PRICE_GROWTH = 1.15;
+var item = {};
+var importValue;
 init();
+document.getElementById("exportButton").onclick = exportSave;
+document.getElementById("importButton").onclick = importSave;
 
 
